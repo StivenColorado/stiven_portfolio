9a3ccf0791cbd15069b6394c6e60eb7667a8b36d
Stiven Colorado <stivenchoo@gmail.com>
2024-12-17 09:25:29 -0500

    vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,15 @@
 import { defineConfig } from 'astro/config';
 
 import tailwind from "@astrojs/tailwind";
+import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'server', // Importante para el despliegue en Vercel
+  adapter: vercel(),
   integrations: [tailwind()],
   redirects: {
     '/Portfolio': '/Portfolio'
   }
 });
+
